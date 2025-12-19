46cde5d38ffa706d9c60874759ff872c128b74d0
Nathan Humphries <nhumphries72@gmail.com>
2025-12-19 14:52:23 -0800

    Attempt to force ipv4

--- decibel_sorter.py
+++ decibel_sorter.py
@@ -11,6 +11,14 @@ import tempfile
 import gc
 from pytube import YouTube
 from pytube.innertube import _default_clients
+import socket
+
+orig_getaddrinfo = socket.getaddrinfo
+
+def getaddrinfo_ipv4(host, port, family=0, type=0, proto=0, flags=0):
+    return orig_getaddrinfo(host, port, socket.AF_INET, type, proto, flags)
+
+socket.getaddrinfo = getaddrinfo_ipv4
 
 max_duration = 300
 cache_dir = "temp_videos"    
